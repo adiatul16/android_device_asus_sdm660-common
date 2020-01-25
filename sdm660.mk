559b95eb7ad89b8a9bf173e6888e02880b7cd100
argraur <argraur@gmail.com>
2020-01-25 22:27:42 +0530

    sdm660-common: Use Pixel charger animation
    Signed-off-by: Aditya <adiatul16@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -278,6 +278,11 @@ PRODUCT_PACKAGES += \
     SecureElement \
     Tag
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
