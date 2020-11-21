4401b111699b5dede894689123ef751468a508bc
StyloGey <stylogey@gmail.com>
2020-11-22 00:24:05 +0530

    sdm660-common: use perfconfigstore instead of properties

--- sdm660.mk
+++ sdm660.mk
@@ -365,7 +365,8 @@ PRODUCT_PACKAGES += \
 
 # QTI Performance
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
+    $(LOCAL_PATH)/configs/perf/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Radio
 PRODUCT_PACKAGES += \
